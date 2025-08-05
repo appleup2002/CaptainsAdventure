63b0e9be8b9ce35f8f2db9ab538390cfd2f80a15
appleup <1783524093@qq.com>
2025-08-05 20:15:59 +0800

    upload other parts

---
+++ Source/workdemo/workdemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "workdemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WORKDEMO_API AworkdemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
