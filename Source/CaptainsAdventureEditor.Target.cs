c8793030a0437977345b22124cbbb00b5cae1fce
appleup2002 <appleup2002@gmail.com>
2026-03-31 15:26:13 +0800

    [feat]: 解决内部报错

--- Source/CaptainsAdventureEditor.Target.cs
+++ Source/CaptainsAdventureEditor.Target.cs
@@ -1,15 +1,15 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Fill out your copyright notice in the Description page of Project Settings.
 
 using UnrealBuildTool;
 using System.Collections.Generic;
 
 public class CaptainsAdventureEditorTarget : TargetRules
 {
-	public CaptainsAdventureEditorTarget( TargetInfo Target) : base(Target)
+	public CaptainsAdventureEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V6;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
-		ExtraModuleNames.Add("CaptainsAdventure");
+
+		ExtraModuleNames.AddRange( new string[] { "CaptainsAdventure" } );
 	}
 }
