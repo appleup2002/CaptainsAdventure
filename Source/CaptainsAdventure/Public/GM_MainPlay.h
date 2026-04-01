f5a961ae3978442c315dc017999b73d1d16140ae
appleup2002 <appleup2002@gmail.com>
2026-04-01 12:40:16 +0800

    [feat]: 解决报错与上传资源

---
+++ Source/CaptainsAdventure/Public/GM_MainPlay.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "GM_MainPlay.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAPTAINSADVENTURE_API AGM_MainPlay : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
